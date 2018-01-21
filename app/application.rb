4be1f866b07108cb77279fa99908d46c3d12e5de
Elle Faraday <faraday.elle@gmail.com>
2018-01-21 01:49:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,23 @@
 class Application
-
+  @@items = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    
+
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      resp.write find_item_by_name(item_name).price
+    else
+      resp.status
     resp.finish
+  end
+
+  def find_item_by_name(name)
+    @@items.each do |item|
+      if item.name == name
+        found = item
+      end
+    end
+    found
   end
 end
